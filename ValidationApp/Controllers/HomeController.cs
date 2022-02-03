6b4e97827e58a77e49f18919cae77cfd3c48420c
kaiogotyacode <kaioreis1153@hotmail.com>
2022-02-03 09:22:23 -0300

    Add project files.

---
+++ ValidationApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationApp.Models;
+
+
+namespace ValidationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            Pessoa pessoa = new Pessoa();
+        
+
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+                
+
+            if (ModelState.IsValid){
+                return View("Resultado", pessoa);
+            }
+
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {   
+            return View(pessoa);
+        }
+  
+    }
+}
---
+++ ValidationApp/Models/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ValidationApp.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "O campo nome deve ser preenchido.")]
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        [Range(0,150,ErrorMessage ="Este campo deve conter números entre 0 à 150")]
+        public int Idade { get; set; }
+
+        [StringLength(20, MinimumLength = 9, ErrorMessage = "O campo Telefone deve conter entre 9 à 20 caracteres")]
+        public string  Telefone { get; set; }
+
+        [StringLength(16,MinimumLength = 6, ErrorMessage ="A senha deve conter entre 6 à 16 caracteres")]
+        public string  Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage ="As senhas não conferem!")]
+        public string  C_Senha { get; set; }
+    }
+}
